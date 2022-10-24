ea8b0cd7976a5fe038d239be5c6e20f9ab3d831e
Carmen Ortigoza <43401320+ZzalyOreo@users.noreply.github.com>
2022-10-24 14:50:09 -0500

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,4 +1,3 @@
-// Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyDFMxPzf8jN-Fs3yV-SXt3YyEl6TDlewQA",
   authDomain: "kiwiter-d023b.firebaseapp.com",
@@ -8,66 +7,50 @@ var firebaseConfig = {
   messagingSenderId: "901924792520",
   appId: "1:901924792520:web:6b812a83958462331a1dfd"
 };
-
+// Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
 
-user_name = localStorage.getItem("user_name");
-room_name = localStorage.getItem("room_name");
+//Añade tus enlaces de Firebase
 
-document.getElementById("user_name").innerHTML = "¡Hola " + user_name + "!";
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "¡Bienvenido" + user_name + "!";
 
 function addRoom() {
   room_name = document.getElementById("room_name").value;
 
   firebase.database().ref("/").child(room_name).update({
-    purpose: "adding room name"
+    purpose: "añadir el nombre de la sala"
   });
 
   localStorage.setItem("room_name", room_name);
-  window.location.replace("kwitter_page.html");
 
+  window.location = "kwitter_page.html";
 }
-
 
 function getData() {
-  firebase.database().ref("/" + room_name).on('value', function (snapshot) {
-    document.getElementById("output").innerHTML = "";
-    snapshot.forEach(function (childSnapshot) {
+  firebase.database().ref("/").on('value', function (snapshot) {
+    document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
       childKey = childSnapshot.key;
-      childData = childSnapshot.val();
-      if (childKey != "purpose") {
-        firebase_message_id = childKey;
-        message_data = childData;
-        //Inicia código
-        console.log(firebase_message_id);
-        console.log(message_data);
-        name = message_data['name'];
-        message = message_data['message'];
-        like = message_data['like'];
-        name_with_tag = "<h4> " + name + "<img class='user_tick' src='tick.png'></h4>";
-        message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
-        like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
-        span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
-
-        row = name_with_tag + message_with_tag + like_button + span_with_tag;
-        document.getElementById("output").innerHTML += row;
-        //Termina código
-      }
+      Room_names = childKey;
+      console.log("Nombre de la sala" + Room_names);
+      row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML += row;
     });
   });
+
 }
+
 getData();
-
-
-function logout() {
-  localStorage.removeItem("user_name");
-  localStorage.removeItem("room_name");
-  window.location.replace("index.html");
-}
-
 
 function redirectToRoomName(name) {
   console.log(name);
   localStorage.setItem("room_name", name);
   window.location = "kwitter_page.html";
+}
+
+function logout() {
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location = "index.html";
 }
